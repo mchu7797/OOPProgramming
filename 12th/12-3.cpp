d9f7fd787c1df8aaee3181849587d793b930cc97
Minseok Chu <mchu7797@gmail.com>
2023-10-31 21:30:02 +0900

    code formatting

--- 12th/12-3.cpp
+++ 12th/12-3.cpp
@@ -40,7 +40,7 @@ int main(void) {
     }
 
     for (int i = 0; i < 26; ++i) {
-        outputStream << (char) i << ":" << count[i] << endl;
+        outputStream << (char) (i + 'A') << ":" << count[i] << endl;
     }
 
     inputStream.close();
